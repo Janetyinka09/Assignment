6b5c3644870d12d73d20fcd095abcad3df6f1b12
Janetyinka09 <jancyalice09@gmail.com>
2024-07-01 04:15:10 -0700

    Update discount.py

--- discount.py
+++ discount.py
@@ -6,8 +6,12 @@ user_card4 = "P"
 cost = int(input("enter the cost of the product")) 
 silver_discount = int((5/100) * cost )
 gold_discount = int((10/100) * cost )
-platinum_discount = int((15/100) * cost)
-if user_input == "1":
+platinum_discount = int((15/100) * cost) 
+if cost>1000:
+    print("you are to pay" , silver_discount) 
+elif cost>2000:
+    print("you are to pay" , gold_discount)
+elif user_input == "1":
     user_card = user_card1 
     print("you are to pay" , cost) 
 elif user_input=="2":
@@ -20,4 +24,4 @@ elif user_input == "4":
     user_card = user_card4
     print("you are to pay" , platinum_discount) 
 else:
-    print("your user card is not valid") 
+    print("your user type is not valid") 
